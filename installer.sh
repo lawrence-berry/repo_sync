de3ada4165364c0ee12aa5f5569380d6d869c69c
Lawrence Berry <lawrence.berry@gmail.com>
2018-12-02 15:17:46 +0000

    fixes

--- installer.sh
+++ installer.sh
@@ -5,18 +5,9 @@ function isInstalled() {
   echo $IS_INSTALLED
 }
 
-# if [[ $(isInstalled) -eq "0" ]]; then
-#   echo "Installing repo_sync ..."
-#   crontab -l > mycron
-#   echo "#repo_sync: Scheduled repo updater" >> mycron
-#   echo "* * * * * `echo $PWD`/sync.sh" >> mycron
-#   sudo crontab -u route mycron
-#   rm mycron
-# fi
-
 if [[ $(isInstalled) -eq "0" ]]; then
-  "installing Repo Sync ..."
-  mv ./repo_sync.plist ~/Library/LaunchAgents/repo_sync.plist
+  echo "installing Repo Sync ..."
+  mv repo_sync.plist ~/Library/LaunchAgents/repo_sync.plist
   launchctl load ~/Library/LaunchAgents/repo_sync.plist
   launchctl start repo_sync
 fi
