22654277a935b26eefe0781d6408ec69c5a52d18
Lawrence Berry <lawrence.berry@gmail.com>
2018-12-02 15:02:29 +0000

    installer

--- installer.sh
+++ installer.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -
 
-# function isInstalled() {
-#   IS_INSTALLED=`crontab -l | grep 'repo_sync' | wc -l`
-#   echo $IS_INSTALLED
-# }
-
+function isInstalled() {
+  IS_INSTALLED=`launchctl list | grep 'repo_sync' | wc -l`
+  echo $IS_INSTALLED
+}
 
 # if [[ $(isInstalled) -eq "0" ]]; then
 #   echo "Installing repo_sync ..."
